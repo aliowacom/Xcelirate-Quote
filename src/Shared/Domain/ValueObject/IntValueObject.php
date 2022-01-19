232312a6a6726f6547eb8e9e6488c255691eba08
Alex Averkiyev <aleksejs.averkijevs@gmail.com>
2022-01-19 18:58:09 +0100

    Shared VO

---
+++ src/Shared/Domain/ValueObject/IntValueObject.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace XcelirateQuote\Shared\Domain\ValueObject;
+
+abstract class IntValueObject
+{
+    public function __construct(protected int $value){}
+
+    public function value(): int
+    {
+        return $this->value;
+    }
+}
